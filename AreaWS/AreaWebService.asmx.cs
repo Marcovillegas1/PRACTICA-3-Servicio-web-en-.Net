ebcae88388134d8ffc1bd32a9d2ec86a63a385fa
Marcovillegas1 <89719028+Marcovillegas1@users.noreply.github.com>
2023-09-23 03:17:12 -0600

    Primera carpeta

---
+++ AreaWS/AreaWebService.asmx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace AreaWS
+{
+    /// <summary>
+    /// Descripción breve de AreaWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class AreaWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public decimal AreaCuadrado(decimal lado1, decimal lado2)
+        {
+            Operaciones o = new Operaciones();
+            return o.cuadrado(lado1, lado2);
+
+        }
+        [WebMethod]
+        public decimal AreaTriangulo(decimal largo, decimal ancho)
+        {
+            Operaciones o = new Operaciones();
+            return o.triangulo(largo, ancho);
+        }
+        [WebMethod]
+        public double AreaCirculo(double radio)
+        {
+            Operaciones o = new Operaciones();
+            return o.circulo(radio);
+        }
+    }
+}
---
+++ AreaWS/Operaciones.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AreaWS
+{
+    public class Operaciones
+    {
+
+        public decimal cuadrado(decimal n1, decimal n2)
+        {
+            return n1 * n2;
+        }
+        public decimal triangulo(decimal n1, decimal n2)
+        {
+            var resultado = n1 * n2;
+            return resultado / 2;
+        }
+        public double circulo(double n1)
+        {
+            double pi = 3.14;
+            double radio2 = n1 * n1;
+            return pi * radio2;
+        }
+    }
+}
